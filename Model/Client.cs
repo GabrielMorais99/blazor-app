0cccd41795fe8c08b58e9f2028fdab0162e0056b
GabrielMorais99 <gabrielmorais80@hotmail.com>
2020-11-22 00:48:02 -0300

    'test'

--- Model/Client.cs
+++ Model/Client.cs
@@ -5,7 +5,11 @@ namespace BlazorApp.Model
  public class Client
  {
   [Required]
-  [StringLength(10, ErrorMessage = "Name is too long.")]
+  [StringLength(14, ErrorMessage = "Name is too long.")]
   public string Name { get; set; }
+  public string Assunto { get; set; }
+  public string Usuario { get; set; }
+  public string Pessoa { get; set; }
+  public string Tipo { get; set; }
  }
 }
